48062e3e2f71c5ab7a15cd9873091185a7dc2d38
Hoàng Linh <linhhv@Hoangs-MacBook-Air.local>
2025-09-06 16:43:55 +0700

    Add AL

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <Analytics />
+  </div>
+</template>
+
+<script setup>
+// Import component Analytics
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
